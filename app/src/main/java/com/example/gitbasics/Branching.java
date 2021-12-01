48f23a7371a95a3ed14cea1de38580338ca8aaab
somanidarshan <somanidarshancoder@gmail.com>
2021-12-01 22:10:34 +0530

    Added Introduction of GIT in MainActivity

---
+++ app/src/main/java/com/example/gitbasics/Branching.java
@@ -0,0 +1,14 @@
+package com.example.gitbasics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Branching extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_branching);
+    }
+}
--- app/src/main/java/com/example/gitbasics/MainActivity.java
+++ app/src/main/java/com/example/gitbasics/MainActivity.java
@@ -10,5 +10,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*Leaning Git and Github for Android Studio
+        Lecture 1: HOW TO ADD PROJECT ON GITHUB
+        Step 1: Go the Github account and make one public repository
+        Step 2: Now Come to your Android Studio and Open your Project in it
+        Step 3: First of all in VCS option select "Enable Version Control integrations"
+        Step 4: Now change the project folder from "Android" to "Project"
+        Step 5: Right Click on Project and select git>>Select add (Ctrl+alt+A)
+        Step 6: Then all the Files will be added.
+        Step 7: Then Again right click on project,git>>select commit directory
+        Step 8: After that go to repo and select <<push>>
+        Step 9: In the next window click on "define remote"
+        Step 10: In the upcoming window paste the url of the repo that you have created
+        Step 11: Then push all the changes and write commit msg
+
+        */
+
+
     }
 }
